92f3d8d2912d510bd14142bdf140248d2f0967f0
Damian Rzeszot <damian.rzeszot@gmail.com>
2024-08-04 22:59:27 +0200

    Initial

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Sugar",
+    platforms: [
+        .iOS(.v17),
+    ],
+    products: [
+        .library(
+            name: "Sugar",
+            targets: [
+                "Sugar"
+            ]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Sugar"
+        ),
+        .testTarget(
+            name: "SugarTests",
+            dependencies: [
+                "Sugar"
+            ]
+        ),
+    ]
+)
